718227ca0c7dd2c21f505a7e6db7186852be8dc6
local V <teslamanager@protonmail.com>
2026-03-18 22:53:23 -0500

    nest g resourse clients/users/auth 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ClientsModule } from './clients/clients.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -17,6 +20,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+
+    UsersModule,
+
+    ClientsModule,
+
+    AuthModule,
 
   ],
   controllers: [],
