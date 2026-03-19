8addeb1b802cf52cb9f03eb60e9745372500cdf7
local V <teslamanager@protonmail.com>
2026-03-18 23:21:58 -0500

    implementa user.entity.ts funcional

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,54 @@
-export class User {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  CreateDateColumn,
+} from 'typeorm';
+
+/**
+ * Enum de roles
+ */
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  JEFE = 'JEFE',
+  VENDEDOR = 'VENDEDOR',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.VENDEDOR,
+  })
+  role: UserRole;
+
+  /**
+   * JEFE del vendedor
+   * - null para ADMIN y JEFE
+   */
+  @ManyToOne(() => User, (user) => user.sellers, {
+    nullable: true,
+  })
+  boss: User;
+
+  /**
+   * Vendedores de un JEFE
+   */
+  @OneToMany(() => User, (user) => user.boss)
+  sellers: User[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
