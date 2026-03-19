35a3f6a7a89c34518c8cf2ee40afaa0a29cff3a3
local V <teslamanager@protonmail.com>
2026-03-18 23:42:10 -0500

    implementa client.entity.ts funcional

--- src/clients/entities/client.entity.ts
+++ src/clients/entities/client.entity.ts
@@ -1,1 +1,73 @@
-export class Client {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('clients')
+export class Client {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  // ----- Datos personales -----
+  @Column()
+  nombres: string;
+
+  @Column()
+  apellidos: string;
+
+  @Column({ nullable: true })
+  dni: string;
+
+  @Column({ nullable: true })
+  numeroCliente: string;
+
+  // ----- Datos comerciales -----
+  @Column({ nullable: true })
+  metodoPago: string; // Cuotas / Contado
+
+  @Column({ nullable: true })
+  metodoSeguimiento: string; // Llamada / Mensaje
+
+  @Column({ default: false })
+  simulacion: boolean;
+
+  @Column({ nullable: true })
+  tipoCliente: string; // A / B / C2
+
+  @Column({ nullable: true })
+  resolucion: string;
+
+  // ----- Estados -----
+  @Column({ default: false })
+  documentacionCompleta: boolean;
+
+  @Column({ nullable: true })
+  referencias: string;
+
+  @Column({ nullable: true })
+  verificacionIdentidad: string;
+
+  @Column({ default: false })
+  facturado: boolean;
+
+  @Column({ default: false })
+  despachado: boolean;
+
+  // ----- Observaciones -----
+  @Column({ type: 'text', nullable: true })
+  observaciones: string;
+
+  // ----- Relaciones -----
+  @ManyToOne(() => User, { nullable: false })
+  creadoPor: User;
+
+  @ManyToOne(() => User, { nullable: false })
+  vendedorAsignado: User;
+
+  @CreateDateColumn()
+  fechaCreacion: Date;
+}
